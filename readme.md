775d9f30ddf7cffe32836af44adff906fdcb297f
Abigail Adegbiji <adegbijiabigail@gmail.com>
2023-11-11 21:24:50 -0600

    publish and remove posts

--- readme.md
+++
@@ -1,6 +0,0 @@
-the markdown files and generated html files for the website.
-
-the static site generator used to generate the website
-
-Highlight: #fc1e7f
-Background: #101017
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use tera::Tera;
 use tera::Context;
 
@@ -47,21 +49,21 @@ fn transpile_markdown(file_contents: &str) -> String {
 struct Post {
     path: String,
     title: String,
-    html: String,
-    markdown: String,
     read_time: String,
     publish_date: String,
+    html: String,
+    markdown: String
 }
 
 impl Post {
-    fn new(title: &str, path: &str, date: &str) -> Self {
+    fn new() -> Self {
         Post {
-            path: String::from(path),
-            title: String::from(title),
-            publish_date: String::from(date),
-            html: String::new(),
-            markdown: String::new(),
-            read_time: String::new(),
+            path: String::new(),  
+            title: String::new(),  
+            read_time: String::new(),  
+            publish_date: String::new(),  
+            html: String::new(),  
+            markdown: String::new(),  
         }
     }
 
@@ -69,16 +71,15 @@ impl Post {
         let markdown = std::fs::read_to_string(&path).unwrap();
         let html = transpile_markdown(&markdown);
 
-        let path_parts: Vec<&str> = path.split(".").collect();
-        let path = path_parts[0].to_owned() + ".html";
-
         let time = chrono::Utc::now();
         let publish_date = time.format("%B %d, %Y").to_string();
 
         let title_line = markdown.lines().next().unwrap();
         let title = title_line[2..].to_string();
 
-        let wpm = 200.0; // Number of words per minute the average person reads
+        let path = title.replace(" ", "_") + ".html";
+
+        let wpm = 200.0; // Number of words the average person reads
         let words = (get_word_count(&markdown) as f64) / wpm;
         let read_time = format!("{} min", words as i32);
 
@@ -88,61 +89,88 @@ impl Post {
 
 #[derive(Serialize)]
 struct Blog {
-    json: Value,
-    posts: Vec<Post>,
-    json_path: String,
-    removed_post: String,
+    posts: HashMap<String, Post>,
+    posts_cache_path: String
 }
 
 impl Blog {
-    fn new(json_path: &str) -> Self {
-        Blog {
-            json: Value::Null,
-            posts: Vec::new(),
-            removed_post: String::new(),
-            json_path: String::from(json_path)
+    fn new(posts_cache_path: &str) -> Self {
+        let mut blog = Blog {
+            posts: HashMap::new(),
+            posts_cache_path: String::from(posts_cache_path)
+        };
+
+        blog.load_archive();
+        blog
+    }
+
+    fn load_archive(&mut self) {
+        let json_str = std::fs::read_to_string(&self.posts_cache_path).unwrap();
+        let json: Value = serde_json::from_str(&json_str).unwrap();
+
+        for (key, value) in json["posts"].as_object().unwrap() {
+            let mut p = Post::new();
+
+            p.title = key.to_string();
+            p.path = value["path"].to_string();
+            p.publish_date = value["publish_date"].to_string();
+            self.posts.insert(key.to_string(), p);
         }
     }
 
-    fn load(&mut self) {
-        let json_str = std::fs::read_to_string(&self.json_path).unwrap();
-        self.json = serde_json::from_str(&json_str).unwrap();
-
-        for (key, value) in self.json["posts"].as_object().unwrap() {
-            if key == &self.removed_post {
-                continue;
-            }
-
-            let p = Post::new(key, &value["path"].to_string(), &value["publish_date"].to_string());
-            self.posts.push(p);
-        }
+    fn save_archive(&self) {
+        let json = serde_json::to_string(&self.posts).unwrap();
+        std::fs::write(&self.posts_cache_path, json).unwrap();
     }
 
-    fn build_post(&mut self, path: &str) {
+    fn remove_post(&mut self, post_title: &str) {
+        self.posts.remove(post_title);
+    }
+
+    fn add_post(&mut self, path: &str) {
         let mut tera = Tera::default();
-        tera.add_template_file("templates/post.template", Some("Post")).unwrap();
+        let template = "templates/post.template";
+        tera.add_template_file(template, Some("Post")).unwrap();
 
         let p = Post::init(path);
         let context = Context::from_serialize(&p).unwrap();
+
         let html = tera.render("Post", &context).unwrap();
         std::fs::write(&p.path, html).unwrap();
 
-        self.posts.push(p);
+        self.posts.insert(p.title.clone(), p);
     }
 
     fn build(&mut self) {
         let mut tera = Tera::default();
-        tera.add_template_file("templates/index.template", Some("Index")).unwrap();
+        let template = "templates/index.template";
+        tera.add_template_file(template, Some("Index")).unwrap();
 
         let mut context = Context::new();
         context.insert("posts", &self.posts);
+
         let html = tera.render("Index", &context).unwrap();
         std::fs::write("index.html", html).unwrap();
     }
 }
 
 fn print_help() {
-    println!("blog info goes here");
+    println!("{}", "
+blog
+-----
+A super simple static site geneator for my blog.
+
+To create or update a post:
+'blog publish example_file.md'
+The first line in the file should be a header containing the post's title. Ex.
+'
+# Post title
+ <The post's content goes here>
+'
+
+To remove a post:
+'blog remove 'Post title''
+    ");
 }
 
 fn main() {
@@ -152,17 +180,17 @@ fn main() {
         return;
     }
 
-    let mut blog = Blog::new("archive.json");
+    let mut blog = Blog::new("assets/posts.json");
 
     if &args[1] == "publish" {
-        blog.build_post(&args[2]);
+        blog.add_post(&args[2]);
     } else if &args[1] == "remove" {
-        blog.removed_post = String::from(&args[2]);
+        blog.remove_post(&args[2]);
     } else {
         print_help();
         return;
     }
 
-    blog.load();
     blog.build();
+    blog.save_archive();
 }
