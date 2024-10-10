5b7dabc5e03e5252c720d0b9265b1baae056461a
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-10-09 21:46:04 -0600

    i don't think we need to traverse the ast in the first place

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net/url"
 	"os"
 	"strings"
 
 	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/ast"
 	"github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
 )
@@ -65,73 +63,9 @@ func parsePostHeader(source string) (PostHeader, error) {
 	return header, nil
 }
 
-func realFilePath(path, sourceFolder, outputBase string) (string, error) {
-	files, err := os.ReadDir(sourceFolder)
-	if err != nil {
-		return "", err
-	}
-
-	for _, entry := range files {
-		if !entry.IsDir() && entry.Name() == path {
-			// Found the path inside the source folder,
-			// return the new file path
-			return fmt.Sprintf("%s/%s", outputBase, path), nil
-		}
-	}
-
-	return "", fmt.Errorf("%s NOT FOUND IN %s", path, sourceFolder)
-}
-
-// TODO: test this
-// TODO: do we even need this?? How are assets actually sourced on github pages???
-func replaceLocalFilePaths(document ast.Node) (ast.Node, error) {
-	var buildError error
-	outputBase := "https://aabiji.github.com"
-	sourceFolder := "src/"
-	assetFolder := "assets/"
-
-	ast.WalkFunc(document, func(node ast.Node, entering bool) ast.WalkStatus {
-		if link, ok := node.(*ast.Link); ok && entering {
-			dest := string(link.Destination)
-
-			// Ignore valid urls
-			_, err := url.ParseRequestURI(dest)
-			if err == nil || len(dest) == 0 {
-				return ast.GoToNext
-			}
-
-			path := ""
-			pathParts := strings.Split(dest, ".")
-			extension := pathParts[len(pathParts)-1]
-			if extension == "md" {
-				// Fix links to other markdown files (blog posts)
-				path, buildError = realFilePath(dest, sourceFolder, outputBase)
-			} else {
-				// Fix links to potential asset files
-				path, buildError = realFilePath(dest, assetFolder, outputBase)
-			}
-
-			if buildError != nil {
-				return ast.Terminate
-			}
-			link.Destination = []byte(path)
-		}
-		return ast.GoToNext
-	})
-
-	return document, buildError
-}
-
 func transpileMarkdown(source string) ([]byte, error) {
 	p := parser.NewWithExtensions(parser.CommonExtensions)
 	document := p.Parse([]byte(source))
-
-	// Apply AST transformations
-	document, err := replaceLocalFilePaths(document)
-	if err != nil {
-		return nil, err
-	}
-
 	options := html.RendererOptions{Flags: html.CommonFlags}
 	renderer := html.NewRenderer(options)
 	return markdown.Render(document, renderer), nil
