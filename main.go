bcbd96fe24832ca3d397584f6d2690449c4b4b12
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-10-09 21:41:11 -0600

    refactor

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net/url"
 	"os"
 	"strings"
-	"text/template"
 
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/ast"
@@ -14,168 +13,156 @@ import (
 	"github.com/gomarkdown/markdown/parser"
 )
 
-func inDirectory(directory, file string) bool {
-	files, err := os.ReadDir(directory)
+func readFile(path string) (string, error) {
+	contents, err := os.ReadFile(path)
 	if err != nil {
-		return false
+		return "", err
+	}
+	return string(contents), nil
+}
+
+func onlyContains(str string, char byte) bool {
+	for i := 0; i < len(str); i++ {
+		if str[i] != char {
+			return false
+		}
+	}
+	return true
+}
+
+type PostHeader struct {
+	Info     map[string]string
+	endIndex int // Index where the post header ends
+}
+
+func parsePostHeader(source string) (PostHeader, error) {
+	header := PostHeader{Info: make(map[string]string, 1)}
+	lines := strings.Split(source, "\n")
+
+	for _, line := range lines {
+		// Ignore empty lines
+		if len(strings.Trim(line, " ")) == 0 {
+			continue
+		}
+
+		// A post header ends with a horizantal line separator
+		header.endIndex += len(line) + 1
+		if onlyContains(line, '-') {
+			break
+		}
+
+		pair := strings.Split(line, ":")
+		if len(pair) != 2 {
+			return PostHeader{}, errors.New("INVALID KEY VALUE")
+		}
+
+		key := strings.Trim(pair[0], " ")
+		key = strings.ToLower(key)
+		value := strings.Trim(pair[1], " ")
+		header.Info[key] = value
+	}
+
+	return header, nil
+}
+
+func realFilePath(path, sourceFolder, outputBase string) (string, error) {
+	files, err := os.ReadDir(sourceFolder)
+	if err != nil {
+		return "", err
 	}
 
 	for _, entry := range files {
-		if !entry.IsDir() && entry.Name() == file {
-			return true
+		if !entry.IsDir() && entry.Name() == path {
+			// Found the path inside the source folder,
+			// return the new file path
+			return fmt.Sprintf("%s/%s", outputBase, path), nil
 		}
 	}
 
-	return false
+	return "", fmt.Errorf("%s NOT FOUND IN %s", path, sourceFolder)
 }
 
-// Get the full path to local file paths
-// TODO: the full paths should be urls
-func getFullPath(path string) ([]byte, error) {
-	pathParts := strings.Split(path, ".")
-	extension := pathParts[len(pathParts)-1]
-
-	// It's a link to another markdown file, check if we can
-	// find it in the sources folder
-	if extension == "md" && inDirectory("src/", path) {
-		return []byte("src/" + path), nil
-	} else if extension == "md" {
-		return nil, fmt.Errorf("%s is not found in src/", path)
-	}
-
-	// At this point it should be an asset
-	if !inDirectory("assets/", path) {
-		return []byte("assets/" + path), nil
-	}
-	return nil, fmt.Errorf("%s is not found in src/", path)
-}
-
+// TODO: test this
+// TODO: do we even need this?? How are assets actually sourced on github pages???
 func replaceLocalFilePaths(document ast.Node) (ast.Node, error) {
 	var buildError error
+	outputBase := "https://aabiji.github.com"
+	sourceFolder := "src/"
+	assetFolder := "assets/"
+
 	ast.WalkFunc(document, func(node ast.Node, entering bool) ast.WalkStatus {
 		if link, ok := node.(*ast.Link); ok && entering {
-			path := string(link.Destination)
-			_, err := url.ParseRequestURI(path)
-			if err == nil {
+			dest := string(link.Destination)
+
+			// Ignore valid urls
+			_, err := url.ParseRequestURI(dest)
+			if err == nil || len(dest) == 0 {
 				return ast.GoToNext
 			}
 
-			fullpath, err := getFullPath(path)
-			if err != nil {
-				buildError = err
+			path := ""
+			pathParts := strings.Split(dest, ".")
+			extension := pathParts[len(pathParts)-1]
+			if extension == "md" {
+				// Fix links to other markdown files (blog posts)
+				path, buildError = realFilePath(dest, sourceFolder, outputBase)
+			} else {
+				// Fix links to potential asset files
+				path, buildError = realFilePath(dest, assetFolder, outputBase)
+			}
+
+			if buildError != nil {
 				return ast.Terminate
 			}
-			link.Destination = fullpath
+			link.Destination = []byte(path)
 		}
 		return ast.GoToNext
 	})
+
 	return document, buildError
 }
 
-type Post struct {
-	Title   string
-	Date    string
-	Content string
-}
+func transpileMarkdown(source string) ([]byte, error) {
+	p := parser.NewWithExtensions(parser.CommonExtensions)
+	document := p.Parse([]byte(source))
 
-// At the top of each markdown source, there will
-// be a header that ends with a markdown line
-func findHeaderSeparator(lines []string) int {
-	offset := 0
-	for lineIndex, line := range lines {
-		isSeparator := true
-		offset += len(line)
-
-		for i := 0; i < len(line); i++ {
-			if line[i] != '-' {
-				isSeparator = false
-				break
-			}
-		}
-
-		if isSeparator {
-			return lineIndex
-		}
+	// Apply AST transformations
+	document, err := replaceLocalFilePaths(document)
+	if err != nil {
+		return nil, err
 	}
 
-	return -1
-}
-
-func parsePost(file []byte) (Post, error) {
-	lines := strings.Split(string(file), "\n")
-	lineIndex := findHeaderSeparator(lines)
-	if lineIndex == -1 {
-		return Post{}, errors.New("EXPECTING HEADER")
-	}
-
-	// Extract yaml like info the struct
-	post := Post{}
-	for _, line := range lines[:lineIndex] {
-		parts := strings.Split(line, ":")
-		key := strings.Trim(parts[0], " ")
-		value := strings.Trim(parts[1], " ")
-		if key == "Title" {
-			post.Title = value
-		} else if key == "Date" {
-			post.Date = value
-		}
-	}
-
-	// Get the remaining content
-	offset := 0
-	for _, line := range lines[:lineIndex+1] {
-		offset += len(line)
-	}
-	offset += len(lines[lineIndex])
-	post.Content = strings.Join(lines[lineIndex:], "\n")
-	return post, nil
-}
-
-// Transpile markdown into html
-// TODO: indentation
-func transpileMarkdown(source []byte) []byte {
-	extensions := parser.CommonExtensions
-	p := parser.NewWithExtensions(extensions)
-
-	document := p.Parse(source)
-	// TODO: move this out
-	//document, err = replaceLocalFilePaths(document)
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	opts := html.RendererOptions{Flags: html.CommonFlags}
-	renderer := html.NewRenderer(opts)
-	return markdown.Render(document, renderer)
+	options := html.RendererOptions{Flags: html.CommonFlags}
+	renderer := html.NewRenderer(options)
+	return markdown.Render(document, renderer), nil
 }
 
 func main() {
-	mdFile, err := os.ReadFile("home.md")
+	source, err := readFile("home.md")
 	if err != nil {
 		panic(err)
 	}
-
-	templateFile, err := os.ReadFile("template.html")
+	header, err := parsePostHeader(source)
 	if err != nil {
 		panic(err)
 	}
-
-	post, err := parsePost(mdFile)
+	htmlSource, err := transpileMarkdown(source[header.endIndex:])
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(string(htmlSource))
 	// TODO: make this less dodgy
-	htmlOutput := transpileMarkdown([]byte(post.Content))
-	post.Content = string(htmlOutput)
-
-	builder := new(strings.Builder)
-	t := template.New("Article")
-	t = template.Must(t.Parse(string(templateFile)))
-	t.Execute(builder, post)
-
-	// Output html
-	err = os.WriteFile("home.html", []byte(builder.String()), 0777)
-	if err != nil {
-		panic(err)
-	}
+	//htmlOutput := transpileMarkdown([]byte(post.Content))
+	//post.Content = string(htmlOutput)
+	//
+	//builder := new(strings.Builder)
+	//t := template.New("Article")
+	//t = template.Must(t.Parse(string(templateFile)))
+	//t.Execute(builder, post)
+	//
+	//// Output html
+	//err = os.WriteFile("home.html", []byte(builder.String()), 0777)
+	//if err != nil {
+	//	panic(err)
+	//}
 }
